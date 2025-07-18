a84b9af0cb4b9ec82fca524e88bd8d9948d03a2e
Annmariyajoby <annmariyajoby2028@cs.sjcetpalai.ac.in>
2025-07-18 21:15:59 +0530

    Add files via upload

---
+++ Month.java
@@ -0,0 +1,47 @@
+package module1;
+import java.util.Scanner;
+public class Month {
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        String monthName;
+	        int year;
+	        int numberOfDays = 0;
+	        System.out.println("Enter the year (YYYY):");
+	        year = sc.nextInt();
+	        System.out.println("Enter the Month Name:");
+	        monthName = sc.next();
+	        switch (monthName.toLowerCase()) {
+	            case "january":
+	            case "march":
+	            case "may":
+	            case "july":
+	            case "august":
+	            case "october":
+	            case "december":
+	                numberOfDays = 31;
+	                break;
+	            case "april":
+	            case "june":
+	            case "september":
+	            case "november":
+	                numberOfDays = 30;
+	                break;
+	            case "february":
+	                if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+	                    numberOfDays = 29;
+	                } else {
+	                    numberOfDays = 28;
+	                }
+	                break;
+
+	            default:
+	                System.out.println("Invalid");
+	                break;
+	        }
+
+	        System.out.println("The number of days in " + monthName + " " + year + " is: " + numberOfDays);
+	        sc.close();
+	    }
+}
+
+   
